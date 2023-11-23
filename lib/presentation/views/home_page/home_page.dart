42baec048ba6c3aa227fa4e46560f23d0a5dc77b
AnuradhaDev96 <anusampath9470@gmail.com>
2023-11-23 19:56:22 +0530

    KY-03 Home page custom scroll view implementation

--- lib/presentation/views/home_page/home_page.dart
+++ lib/presentation/views/home_page/home_page.dart
@@ -56,11 +56,10 @@ class HomePage extends StatelessWidget {
                         height: 88,
                         margin: const EdgeInsets.only(bottom: 13),
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          image: DecorationImage(
-                            image: AssetImage(category.source),
-                          )
-                        ),
+                            borderRadius: BorderRadius.circular(16),
+                            image: DecorationImage(
+                              image: AssetImage(category.source),
+                            )),
                       ),
                       Text(
                         category.text,
@@ -78,6 +77,53 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
+          SliverToBoxAdapter(
+            child: Container(
+              margin: const EdgeInsets.only(left: 20, right: 23),
+              padding: const EdgeInsets.only(left: 24, top: 19, bottom: 24, right: 22),
+              decoration: BoxDecoration(
+                color: AppColors.indigo1.withOpacity(0.71),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Basic Yoga',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.sizeOf(context).width * 0.62,
+                        child: const Text(
+                          'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                          maxLines: 2,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SvgPicture.asset(Assets.rightArrow, width: 20, height: 24),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
--- lib/utils/constants/assets.dart
+++ lib/utils/constants/assets.dart
@@ -5,6 +5,7 @@ abstract class Assets {
 
   // svg
   static const leftArrow = 'assets/svg/back_arrow_box.svg';
+  static const rightArrow = 'assets/svg/right_arrow.svg';
   static const errorFlushbarIcon = 'assets/svg/error_flushbar_icon.svg';
   static const successFlushbarIcon = 'assets/svg/success_flushbar_icon.svg';
   static const logoutIcon = 'assets/svg/logout_icon.svg';
