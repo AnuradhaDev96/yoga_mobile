d83a68b2eb4a87cfd1be853456930f208fb5720d
AnuradhaDev96 <anusampath9470@gmail.com>
2023-11-24 09:51:41 +0530

    KY-04 Lesson list page implementation

--- lib/presentation/views/lessons_list_page/lessons_list_page.dart
+++ lib/presentation/views/lessons_list_page/lessons_list_page.dart
@@ -26,130 +26,147 @@ class _LessonsListPageState extends State<LessonsListPage> {
           Image.asset('assets/png/temp_image.png', width: MediaQuery.sizeOf(context).width, fit: BoxFit.fitWidth)
         ],
       ),
-      bottomSheet: Container(
-        height: _isLessonPlayMode ? MediaQuery.sizeOf(context).height * 0.2 : MediaQuery.sizeOf(context).height * 0.54,
-        width: MediaQuery.sizeOf(context).width,
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(16),
-              topLeft: Radius.circular(16),
-            )),
-        child: CustomScrollView(
-          // controller: _bottomScrollController,
-          physics: const NeverScrollableScrollPhysics(),
-          slivers: [
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8, bottom: 12),
-                child: Center(
-                  child: Container(
-                    width: 48,
-                    height: 5,
-                    decoration: BoxDecoration(color: AppColors.grey4, borderRadius: BorderRadius.circular(100)),
+      bottomSheet: Material(
+        surfaceTintColor: Colors.transparent,
+        color: Colors.white,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(16),
+            topLeft: Radius.circular(16),
+          ),
+        ),
+        child: AnimatedSize(
+          duration: const Duration(milliseconds: 1400),
+          curve: Curves.fastOutSlowIn,
+          alignment: Alignment.topCenter,
+          child: Container(
+            height:
+                _isLessonPlayMode ? MediaQuery.sizeOf(context).height * 0.35 : MediaQuery.sizeOf(context).height * 0.54,
+            width: MediaQuery.sizeOf(context).width,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(16),
+                topLeft: Radius.circular(16),
+              ),
+            ),
+            child: CustomScrollView(
+              // controller: _bottomScrollController,
+              physics: const NeverScrollableScrollPhysics(),
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 8, bottom: 12),
+                    child: Center(
+                      child: Container(
+                        width: 48,
+                        height: 5,
+                        decoration: BoxDecoration(color: AppColors.grey4, borderRadius: BorderRadius.circular(100)),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 24, right: 14, bottom: 21),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        '${widget.sessionData.title ?? 'N/A'} ${widget.sessionData.category ?? 'N/A'}',
-                        style: const TextStyle(
-                          color: AppColors.black1,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      '${widget.sessionData.lessons?.length ?? '0'} lesson${widget.sessionData.lessons?.length == 1 ? '' : 's'}',
-                      style: const TextStyle(
-                        color: AppColors.indigo1,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SliverFillRemaining(
-              child: (widget.sessionData.lessons != null && widget.sessionData.lessons!.isNotEmpty)
-                  ? ListView.separated(
-                      shrinkWrap: true,
-                      padding: const EdgeInsets.only(bottom: 30),
-                      itemBuilder: (context, index) {
-                        var lessonData = widget.sessionData.lessons![index];
-
-                        return GestureDetector(
-                          onTap: () {
-                            _switchToLessonPlayMode();
-                          },
-                          child: Container(
-                            padding: const EdgeInsets.fromLTRB(24, 12, 21, 12),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.15),
-                                  offset: const Offset(0, 3),
-                                  spreadRadius: 0,
-                                  blurRadius: 10,
-                                )
-                              ],
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 24, right: 14, bottom: 21),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Flexible(
+                          child: Text(
+                            '${widget.sessionData.title ?? 'N/A'} ${widget.sessionData.category ?? 'N/A'}',
+                            style: const TextStyle(
+                              color: AppColors.black1,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
                             ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
+                          ),
+                        ),
+                        Text(
+                          '${widget.sessionData.lessons?.length ?? '0'} lesson${widget.sessionData.lessons?.length == 1 ? '' : 's'}',
+                          style: const TextStyle(
+                            color: AppColors.indigo1,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SliverFillRemaining(
+                  child: (widget.sessionData.lessons != null && widget.sessionData.lessons!.isNotEmpty)
+                      ? ListView.separated(
+                          shrinkWrap: true,
+                          padding: const EdgeInsets.only(bottom: 30),
+                          itemBuilder: (context, index) {
+                            var lessonData = widget.sessionData.lessons![index];
+
+                            return GestureDetector(
+                              onTap: () {
+                                _switchToLessonPlayMode();
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.fromLTRB(24, 12, 21, 12),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(0.15),
+                                      offset: const Offset(0, 3),
+                                      spreadRadius: 0,
+                                      blurRadius: 10,
+                                    )
+                                  ],
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Image.asset(Assets.lessonAvatar, width: 40, height: 40),
-                                    const SizedBox(width: 12),
-                                    Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                    Row(
                                       children: [
-                                        Text(
-                                          lessonData.title ?? 'N/A',
-                                          style: const TextStyle(
-                                            fontSize: 16,
-                                            color: AppColors.black1,
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: MediaQuery.sizeOf(context).width * 0.6,
-                                          child: Text(
-                                            lessonData.description ?? 'N/A',
-                                            overflow: TextOverflow.clip,
-                                            maxLines: 1,
-                                            style: const TextStyle(
-                                              fontSize: 13,
-                                              color: Color(0xFF7B7F82),
+                                        Image.asset(Assets.lessonAvatar, width: 40, height: 40),
+                                        const SizedBox(width: 12),
+                                        Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              lessonData.title ?? 'N/A',
+                                              style: const TextStyle(
+                                                fontSize: 16,
+                                                color: AppColors.black1,
+                                              ),
                                             ),
-                                          ),
+                                            SizedBox(
+                                              width: MediaQuery.sizeOf(context).width * 0.6,
+                                              child: Text(
+                                                lessonData.description ?? 'N/A',
+                                                overflow: TextOverflow.clip,
+                                                maxLines: 1,
+                                                style: const TextStyle(
+                                                  fontSize: 13,
+                                                  color: Color(0xFF7B7F82),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
                                         ),
                                       ],
                                     ),
+                                    SvgPicture.asset(Assets.lessonCardPlayIcon, width: 27, height: 27),
                                   ],
                                 ),
-                                SvgPicture.asset(Assets.lessonCardPlayIcon, width: 27, height: 27),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                      separatorBuilder: (context, index) => const SizedBox(height: 23),
-                      itemCount: widget.sessionData.lessons!.length,
-                    )
-                  : const Center(child: Text('No Lessons available')),
+                              ),
+                            );
+                          },
+                          separatorBuilder: (context, index) => const SizedBox(height: 23),
+                          itemCount: widget.sessionData.lessons!.length,
+                        )
+                      : const Center(child: Text('No Lessons available')),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
